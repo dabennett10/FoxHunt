d4ffab0ac588b0342eb4951a0105898b7a5619f8
David Bennett <dabennett10@gmail.com>
2022-11-18 09:30:54 -0500

    Initial Commit

---
+++ FoxHunt/FoxHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoxHuntApp.swift
+//  FoxHunt
+//
+//  Created by David Bennett on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct FoxHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
